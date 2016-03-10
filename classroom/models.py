9d9beb94a6686699ad64e32e68c0fe8cbf9c47d6
worminfo <worminfo@gmail.com>
2016-03-10 22:25:54 +0800

    model classroom update

--- classroom/models.py
+++ classroom/models.py
@@ -2,11 +2,29 @@ from django.db import models
 
 from user.models import User
 
+class User_assignment(models.Model):
+    pass
+
+class Classroom(models.Model):
+    pass
+
+class Announce(models.Model):
+    pass
+
 class Assignment(models.Model):
     pass
 
-class Quiz(models.Model):
+class Exercise(models.Model):
     pass
 
-class Materials(models.Model):
-	pass
+class Note(models.Model):
+    pass
+
+class Material_type(models.Model):
+    pass
+
+class Material_ext(models.Model):
+    pass
+
+class Material(models.Model):
+    pass
--- inbox/models.py
+++ inbox/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+from classroom.models import Classroom
+
+class Inbox_content(models.Model):
+    title = models.CharField(max_length=255, default="No title")
+    content = models.TextField(default="")
+
+class Inbox(models.Model):
+    send_datetime = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender_profile')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver_profile')
+    content = models.ForeignKey(Inbox_content, on_delete=models.CASCADE)
+
+class inbox_classroom(models.Model):
+    inbox = models.ForeignKey(Inbox, on_delete=models.CASCADE)
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
