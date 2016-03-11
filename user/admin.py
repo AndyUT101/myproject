0ba084d2857a3fe9d829d13db74167d7f212988b
worminfo <worminfo@gmail.com>
2016-03-10 23:21:11 -0500

    admin panel update

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(Permission_meta)
+admin.site.register(Permission)
+admin.site.register(Session)
+admin.site.register(Parent_contact)
+
+
+admin.site.register(Class_code)
+admin.site.register(Class_assignment)
