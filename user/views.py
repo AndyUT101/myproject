5824ac34c062551c8d773b8453be6f61e9fcbf63
worminfo <worminfo@gmail.com>
2016-03-11 11:07:35 +0800

    View init testing

--- user/views.py
+++ user/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 #https://djangogirlstaipei.gitbooks.io/django-girls-taipei-tutorial/content/django/views_and_urlconfs.html
+
+def index(request):
+    return HttpResponse('Index')
+
+def login(request):
+    return HttpResponse('Login page.')
+
+def logout(request):
+    return HttpResponse('Logout page.')
