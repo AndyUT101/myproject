0ce73b22f8c54743de651e5262e60cd4fe295b54
worminfo <wormcih@utrepository.local>
2016-03-12 16:33:16 +0800

    mini update

--- user/views.py
+++ user/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from passlib.hash import md5_crypt, bcrypt
 
+from django.contrib.auth.hashers import check_password, make_password
+
 from .models import User
 
 import random, string
@@ -28,6 +30,14 @@ def index(request):
     return render(request, 'login.html')
 
 def login(request):
+
+    """
+
+    from django.contrib.auth.hashers import check_password, make_password
+
+make_password("password", hasher='bcrypt_sha256')
+check_password('password', 'bcrypt_sha256$$2b$12$VADBCPlCt0wMa2zjUgaXl.jnrvoNt/kgjtRY7i./fo0V2IEp1l6f2')
+    """
     # Review login session status
     already_logged = False
     if 'token' in request.session:
@@ -42,13 +52,15 @@ def login(request):
         pass
         # no need to process to db
 
-    hashed_password = bcrypt.encrypt(login_password, rounds=8)
-
     try:
-        user_auth = User.objects.get(username = login_account, password_hash = hashed_password)
+        user_object = User.objects.get(username = login_account)
     
     except ObjectDoesNotExist:
         return redirect('index')
+
+    if not check_password(login_password, user_object.password_hash):
+        return redirect('index')
+
 
     # Assign session token
     if not already_logged:
