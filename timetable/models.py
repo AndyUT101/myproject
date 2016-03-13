6a368101e97c6a3d188bd134dd668da06f7bd843
worminfo <worminfo@gmail.com>
2016-03-14 01:26:42 +0800

    insert str description

--- timetable/models.py
+++ timetable/models.py
@@ -6,9 +6,15 @@ from user.models import User
 
 class Time_meta(models.Model):
     time_obj = models.DateTimeField(default=datetime.datetime.now(), unique=True)
+    
+    def __str__(self):
+        return self.time_obj
 
 class Event_terms(models.Model):
     event_term = models.CharField(max_length=255, default="")
+
+    def __str__(self):
+        return self.event_term
 
 class Activity_event(models.Model):
     location = models.CharField(max_length=255, default="")
@@ -17,14 +23,23 @@ class Activity_event(models.Model):
     start_date = models.ForeignKey(Time_meta, on_delete=models.CASCADE, related_name='start_time_profile', default="")
     end_date = models.ForeignKey(Time_meta, on_delete=models.CASCADE, related_name='end_time_profile', default="")
 
+    def __str__(self):
+        return self.activity_name
+
 class Calendar(models.Model):
     name = models.CharField(max_length=255, default="")
     description = models.TextField(default="")
+
+    def __str__(self):
+        return self.name
 
 class Timetable(models.Model):
     event = models.ForeignKey(Activity_event, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     calender = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.event
 
 
 
