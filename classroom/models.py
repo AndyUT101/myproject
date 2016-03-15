789d491bc72dc8555328fa3688423bc73c16c72c
worminfo <worminfo@gmail.com>
2016-03-15 02:22:26 -0400

    create new app, attendance

--- classroom/models.py
+++ classroom/models.py
@@ -32,7 +32,7 @@ class Announce(models.Model):
     classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, default="")
     title = models.CharField(max_length=255, default="")
     content = models.TextField(default="")
-    announce_date = models.DateTimeField(default=timezone.now(),blank=True)
+    announce_date = models.DateTimeField(default=timezone.now, blank=True)
 
 class Assignment_pool(models.Model):
     """
@@ -58,14 +58,14 @@ class Assignment(models.Model):
         ('T', 'Text'),
     )
     handin_method = models.CharField(max_length=1, choices=METHOD_CHOICE, default="D")
-    deadline_datetime = models.DateTimeField(default=timezone.now(),blank=True)
+    deadline_datetime = models.DateTimeField(default=timezone.now, blank=True)
     fullmark = models.PositiveSmallIntegerField(default=100)
 
 
 class User_assignment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
     assigment = models.ForeignKey(Assignment, on_delete=models.CASCADE, default="")
-    assign_date = models.DateTimeField(default=timezone.now(),blank=True)
+    assign_date = models.DateTimeField(default=timezone.now, blank=True)
 
     ROLE_CHOICES = (
         ('STU', 'Student'),
@@ -93,7 +93,7 @@ class Note(models.Model):
     """
     title = models.CharField(max_length=255, default="")
     content = models.TextField(default="")
-    publish_datetime = models.DateTimeField(default=timezone.now(),blank=True)
+    publish_datetime = models.DateTimeField(default=timezone.now, blank=True)
 
     STATUS_CHOICE = (
         ('P', 'Published'),
@@ -124,6 +124,6 @@ class Material_ext(models.Model):
 
 class Material(models.Model):
     uploader = models.ForeignKey(User, on_delete=models.CASCADE, default="")
-    create_date = models.DateTimeField(default=timezone.now(),blank=True)
+    create_date = models.DateTimeField(default=timezone.now, blank=True)
     classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, default="")
     path = models.FileField(upload_to='uploads/%Y/%m/%d/', default="")
