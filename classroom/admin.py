5a9400d6883d02559b881bd564219fe7e3c0f35e
worminfo <worminfo@gmail.com>
2016-03-16 20:56:52 +0800

    admin panel update

--- classroom/admin.py
+++ classroom/admin.py
@@ -4,3 +4,11 @@ from .models import *
 
 admin.site.register(Classroom)
 admin.site.register(Announce)
+admin.site.register(Assignment)
+admin.site.register(User_assignment)
+admin.site.register(Assignment_pool)
+admin.site.register(Note)
+admin.site.register(Classroom_note)
+admin.site.register(Material_type)
+admin.site.register(Material_ext)
+admin.site.register(Material)
