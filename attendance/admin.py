03f241cba93c76952238b4b054e51271a30eeae2
worminfo <worminfo@gmail.com>
2016-03-16 20:41:31 +0800

    admin update

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Attandance)
+admin.site.register(Rule)
+admin.site.register(Applied_rule)
