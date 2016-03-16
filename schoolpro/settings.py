99e25ffd907752f5d11037a9ce8c2f5661a3dccd
worminfo <worminfo@gmail.com>
2016-03-16 15:20:50 +0800

    setting test

--- schoolpro/settings.py
+++ schoolpro/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(PROJECT_ROOT, 'templates')],
-        'APP_DIRS': True,
+        # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
