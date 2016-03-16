0c56d8fb8854cf8dc88a75bfa7c6cab29cec2bbc
worminfo <worminfo@gmail.com>
2016-03-16 11:12:16 +0800

    model report update

--- report/models.py
+++ report/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 from attendance.models import Attandance
 from user.models import User
+
 
 class Report_type(models.Model):
     title = models.CharField(max_length=255, default="", unique=True)
     description = models.TextField(default="")
     layout_json = models.TextField(default="")
+
+class Report(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
+    generate_datetime = models.DateTimeField(default=timezone.now)
+    report_type = models.ForeignKey(Report_type, on_delete=models.CASCADE, default="")
