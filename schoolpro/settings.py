95a5171d164402e8dd59a5770d12b14db8d58bf5
worminfo <worminfo@gmail.com>
2016-03-16 14:59:43 +0800

    set custom template location

--- schoolpro/settings.py
+++ schoolpro/settings.py
@@ -76,6 +76,16 @@ TEMPLATES = [
     },
 ]
 
+# Assign Template path
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates').replace('\\','/'),
+)
+
 WSGI_APPLICATION = 'schoolpro.wsgi.application'
 
 
