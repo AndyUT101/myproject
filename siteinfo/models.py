8bc5f83200632e399faeeb9eef96548d3671260c
worminfo <worminfo@gmail.com>
2016-03-17 12:24:12 -0400

    siteinfo added

---
+++ siteinfo/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Schoolinfo(models.Model):
+    infokey = models.CharField(max_length=255, default='', unique=True)
+    infoval = models.CharField(max_length=255, default='')
+
+class Sitemap(models.Model):
+    title = models.CharField(max_length=255, default='')
+    level = models.PositiveSmallIntegerField(default=1)
+    url_route = models.CharField(max_length=255, default='')
+
