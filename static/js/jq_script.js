3ab8a7ab9aca9ee18b41c16c1a37cc4d64821fcf
worminfo <worminfo@gmail.com>
2016-03-17 17:18:38 +0800

    design update

--- static/js/jq_script.js
+++ static/js/jq_script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-	// Determine recent inbox span exists
-	if ($('span.recent_inbox').length > 0) 
-		$('span.recent_inbox').html(twemoji.parse('\uD83D\uDCE8') + $('span.recent_inbox').text());
+	// Determine recent inbox li exists
+	if ($('li.recent_inbox').length > 0) {
+		var inbox_count = parseInt($('li.recent_inbox').text());
+		$('li.recent_inbox').html(twemoji.parse('\uD83D\uDCE8') + inbox_count);
+	}
 
 });
