7568bc0708e0a6b1aee65d3494c9c2b9a3c44468
worminfo <worminfo@gmail.com>
2016-03-25 22:10:39 +0800

    update

--- user/forms.py
+++ user/forms.py
@@ -5,7 +5,7 @@ from .models import *
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ["username", "firstname", "lastname", "password_hash", "sex_code", "card_id", "strn_code", "sams_code", "jupas_app_code", "barcode", "birth_date", "hkid_card", "address", "phone", "mobile", "email", "national", "location_of_birth", "intake_date"]
+        fields = '__all__'
         widgets = {
             'password_hash': forms.PasswordInput(),
         }
