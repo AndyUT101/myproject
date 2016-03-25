9359b5cd399c249a484d0d7a2e77f84776a78d8e
worminfo <worminfo@gmail.com>
2016-03-26 03:16:10 +0800

    update

--- user/urls.py
+++ user/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^delete/$', views.remove_user, name='remove_user'),
     url(r'^add/$', views.add_user, name='add_user'),
 
-    url(r'^modify/(?P<specific_id>[0-9]+)$', views.modify_user, name='modify_user'),
+    url(r'^modify/(?P<username>.+)/$', views.modify_user, name='modify_user'),
     url(r'^$', views.list_user, name='list_user'),
     url(r'^(?P<user>.+)/$', views.view_user, name='view_user'),
 ]
