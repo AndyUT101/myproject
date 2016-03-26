320ed39e1b0b7ef48d56a031e997605612b06947
worminfo <worminfo@gmail.com>
2016-03-26 12:43:44 +0800

    test

--- user/forms.py
+++ user/forms.py
@@ -8,6 +8,7 @@ class UserForm(ModelForm):
         fields = '__all__'
         widgets = {
             'password_hash': forms.PasswordInput(),
+
         }
 
     def clean_password_hash(self):
@@ -19,6 +20,7 @@ class UserForm(ModelForm):
 
 class UsermodForm(UserForm):
 
+    """
     class Meta:
     	model = User
         fields = '__all__'
@@ -26,6 +28,19 @@ class UsermodForm(UserForm):
             'password_hash': forms.PasswordInput(),
         }
         exclude = ('username', )
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(UsermodForm, self).__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)
+        if instance and instance.pk:
+            self.fields['username'].widget.attrs['readonly'] = True
+
+    def clean_username(self):
+        if self.instance:
+            return self.instance.username
+        else:
+            return self.fields['username']
 
     def clean_password_hash(self):
         data = self.cleaned_data['password_hash']
@@ -35,8 +50,3 @@ class UsermodForm(UserForm):
                 raise forms.ValidationError('The password should as least 8 characters long.')
 
         return data
-    """
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        return username
-    """
