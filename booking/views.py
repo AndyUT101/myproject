4150149782783d6ae3a93f29aa59ccc019213f04
worminfo <worminfo@gmail.com>
2016-03-26 23:45:16 +0800

    classroom testing

--- booking/views.py
+++ booking/views.py
@@ -5,11 +5,23 @@ from facilities.models import Facilities, Room
 from user.utils import user_alreadyloggedin, get_userrole
 from siteinfo.views import site_topnav
 
+from .forms import ClassroomForm
+
 # helper function
 def room_booked(facilities_obj):
     pass
     # return true false
 
 def book_room(request):
-    pass
+	
+    return render(request, 'home.html', {
+            'page_title': 'Add user',
+            'page_header': 'Add user',
+            'topnav': site_topnav(get_userrole(request.session['user'])['level']),
+            'template': 'form',
+            'content': {
+                'form': ClassroomForm().as_ul(),
+                'submit_url': 'user:add_user',
+            },
+        })
 
---
+++ classroom/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from django import forms
+from .models import *
+
+class ClassroomForm(ModelForm):
+    class Meta:
+    	model = Classroom
+        fields = '__all__'
