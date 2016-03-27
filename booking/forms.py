18d0dc80a8ee433af31bd10f8a3349708c46be30
worminfo <worminfo@gmail.com>
2016-03-27 13:03:41 +0800

    test form verify

--- booking/forms.py
+++ booking/forms.py
@@ -1,9 +1,24 @@
 from django.forms import ModelForm
 from django import forms
-from .models import *
+from .utils import occupied_lesson
+
+from datetime import datetime, time, timedelta
 
 class BookingForm(ModelForm):
     class Meta:
         model = Booking
         fields = '__all__'
         exclude = ('user', )
+
+    def clean(self):
+        cleaned_data = super(Booking, self).clean()
+        clean_startlesson = cleaned_data.get('start_lesson')
+        clean_endlesson = cleaned_data.get('end_lesson')
+
+        if clean_startlesson and clean_endlesson:
+            occupied_id = occupied_lesson()
+	        if clean_startlesson in occupied_id: or clean_endlesson in occupied_lesson:
+                raise forms.ValidationError(
+                    "Select timeslot is already booked by other!",
+                    "Please check and resubmit again."
+                )
