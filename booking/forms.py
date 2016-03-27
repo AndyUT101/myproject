1541c5d7be5e9bef26680aa4f03296651595e794
worminfo <worminfo@gmail.com>
2016-03-27 13:21:11 +0800

    fix

--- booking/forms.py
+++ booking/forms.py
@@ -15,9 +15,9 @@ class BookingForm(ModelForm):
         cleaned_data = super(BookingForm, self).clean()
         clean_bookdate = cleaned_data.get('book_date')
         clean_facility = cleaned_data.get('facility')
-        clean_startlesson = cleaned_data.get('start_lesson')
-        clean_endlesson = cleaned_data.get('end_lesson')
-        raise clean_startlesson
+        clean_startlesson = cleaned_data.get('start_lesson').pk
+        clean_endlesson = cleaned_data.get('end_lesson').pk
+
         if clean_startlesson and clean_endlesson:
             occupied_id = occupied_lesson_id(clean_bookdate, clean_facility)
             if clean_startlesson in occupied_id or clean_endlesson in occupied_id:
