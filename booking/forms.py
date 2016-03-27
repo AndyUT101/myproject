503e1e67297e6f87fb97ae1cecfb9b027b59b823
worminfo <worminfo@gmail.com>
2016-03-27 13:04:17 +0800

    indentation fix

--- booking/forms.py
+++ booking/forms.py
@@ -17,7 +17,7 @@ class BookingForm(ModelForm):
 
         if clean_startlesson and clean_endlesson:
             occupied_id = occupied_lesson()
-	        if clean_startlesson in occupied_id: or clean_endlesson in occupied_lesson:
+            if clean_startlesson in occupied_id: or clean_endlesson in occupied_lesson:
                 raise forms.ValidationError(
                     "Select timeslot is already booked by other!",
                     "Please check and resubmit again."
