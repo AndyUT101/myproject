566f0b2c587112605bcce870c28c2fd9274040c7
worminfo <worminfo@gmail.com>
2016-03-27 15:46:30 +0800

    test

--- facilities/views.py
+++ facilities/views.py
@@ -7,11 +7,38 @@ from django.core.exceptions import ObjectDoesNotExist
 from user.utils import user_alreadyloggedin, get_userrole
 from siteinfo.views import site_topnav
 
+from .models import Facilities, Room
+
 def list_facilities_floor(request):
-    pass
+
     # case 1: return all facilities with floor
     # case 2: return specific floor facilities
     # case 3: return facilities status
+
+    # 1. Check permission
+    if not user_alreadyloggedin(request):
+        return HttpResponseRedirect(reverse('index'))
+
+    user = User.objects.get(username = request.session['user'])
+    if not review_permission(user, 'view:facilites'):
+        return HttpResponseRedirect(reverse('index_home'))
+
+    facilities_obj = Facilities.objects.all().order_by('room')
+
+    return render(request, 'home.html', {
+        'page_title': 'Welcome home!',
+        'page_header': 'Good to seeing you, ',
+        'topnav': site_topnav(get_userrole(request.session['user'])['level']),
+        'template': 'list',
+        'content': {   
+            'list': {
+                'name': 'booking',
+                'body': facilities_obj,
+            },
+        },
+    })
+
+
 
 def add_facilities(request):
     pass
