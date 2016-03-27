8c11fc5c45936fcbcdce0595797edac497dca59c
worminfo <worminfo@gmail.com>
2016-03-27 18:10:39 +0800

    test customize form

--- inbox/forms.py
+++ inbox/forms.py
@@ -1,7 +1,17 @@
+from django import forms
 from django.forms import ModelForm
 from .models import *
 
+"""
 class ComposeForm(ModelForm):
     class Meta:
         model = Inbox
         fields = ["receiver", "content"]
+
+"""
+
+class ComposeForm(forms.Form):
+    sender = forms.CharField(max_length=255)
+    title = forms.CharField(max_length=255)
+    content = forms.TextField(required=False)
+
