0377885b13e05c20c97edddd24e098bf5e4ae511
worminfo <worminfo@gmail.com>
2016-03-27 15:14:26 +0800

    update

--- booking/forms.py
+++ booking/forms.py
@@ -15,12 +15,12 @@ class BookingForm(ModelForm):
         cleaned_data = super(BookingForm, self).clean()
         clean_bookdate = cleaned_data.get('book_date')
         clean_facility = cleaned_data.get('facility')
-        clean_startlesson = cleaned_data.get('start_lesson').pk
-        clean_endlesson = cleaned_data.get('end_lesson').pk
+        clean_startlesson = cleaned_data.get('start_lesson')
+        clean_endlesson = cleaned_data.get('end_lesson')
 
         if clean_bookdate and clean_facility and clean_startlesson and clean_endlesson:
             occupied_id = occupied_lesson_id(clean_bookdate, clean_facility)
-            if clean_startlesson in occupied_id or clean_endlesson in occupied_id:
+            if clean_startlesson.pk in occupied_id or clean_endlesson.pk in occupied_id:
                 raise forms.ValidationError(
                     "Select timeslot is already booked by other! Please check and resubmit again."
                 )
