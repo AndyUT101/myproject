d3736335b569ccb35f7f7aac9ed81b570fa3b0c8
worminfo <worminfo@gmail.com>
2016-03-28 12:18:02 +0800

    attendance view

--- attendance/views.py
+++ attendance/views.py
@@ -8,6 +8,10 @@ from user.utils import user_alreadyloggedin, get_userrole
 from siteinfo.views import site_topnav
 
 import json
+from datetime import datetime
+
+from .models import *
+from user.models import User
 
 # Create your views here.
 def rule_list(request):
@@ -21,8 +25,35 @@ def rollcall(request):
         response_data['message'] = 'Please use POST to send the rollcall data'
 
     elif request.method == 'POST':
-        response_data['status'] = False
-        response_data['message'] = 'No data received'
+    	card_id = request.POST.get('card_id', '')
+    	token = 'b8293a1035a1c929105ea870ecbef6f8'
+    	received_token = request.POST.get('token', '') == token
+
+        response_data['status'] = False
+        response_data['message'] = 'Unknown situation'
+
+        if not card_id:
+            response_data['status'] = False
+            response_data['message'] = 'No data received'
+
+        
+        if not received_token:
+            response_data['status'] = False
+            response_data['message'] = 'Illegal access'
+
+        if received_token and card_id:
+            try:
+                user = User.objects.get(card_id=card_id)
+            except ObjectDoesNotExist:
+                response_data['status'] = False
+                response_data['message'] = 'No user record'
+
+            Attandance(user=user).save()
+
+            response_data['status'] = True
+            response_data['message'] = 'Attandance logged'
+            response_data['user'] = user.username
+            response_data['datetime'] = datetime.now()
 
 
     return HttpResponse(json.dumps(response_data), content_type="application/json")
