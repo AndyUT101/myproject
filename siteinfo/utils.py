76fa997206f718ed804b94e76a33b2554d5ab726
worminfo <worminfo@gmail.com>
2016-03-29 23:36:13 +0800

    zz

--- siteinfo/utils.py
+++ siteinfo/utils.py
@@ -1,23 +1,15 @@
 def site_option():
     option = (
-        ({
-            'name': 'SiteNav editor',
-            'url': 'siteinfo:sitemap_view',
-        }),
+        ({'name': 'SiteNav editor',
+            'url': 'siteinfo:sitemap_view'}),
 
-        ({
-            'name': 'Classroom management',
-            'url': 'classroom:manage',
-        }),
+        ({'name': 'Classroom management',
+            'url': 'classroom:manage'}),
 
-        ({
-            'name': 'User management',
-            'url': 'user',
-        }),
+        ({'name': 'User management',
+            'url': 'user'}),
 
-        ({
-            'name': 'Class management',
-                'url': 'user',
-        }),
+        ({'name': 'Class management',
+            'url': 'user'}),
     )
     return option
