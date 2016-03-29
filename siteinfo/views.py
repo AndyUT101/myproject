ed934fd991995b321d09f3cc8ba1f1f4d42f1064
worminfo <worminfo@gmail.com>
2016-03-29 23:42:17 +0800

    sdsd

--- siteinfo/views.py
+++ siteinfo/views.py
@@ -25,7 +25,7 @@ def site_overview(request):
 
     return render(request, 'home.html', {
         'page_header': page_title,
-        'template': 'testing', # operation, list,
+        'template': 'list', # operation, list,
         'topnav': site_topnav(get_userrole(request.session['user'])['level']),
         'content': {
             'list': {
