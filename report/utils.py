45e537e3e9cdc2d55f29de57469619fea41dccde
worminfo <worminfo@gmail.com>
2016-04-01 05:14:47 +0800

    xxx

--- report/utils.py
+++ report/utils.py
@@ -1,8 +1,6 @@
 from user.models import Class_code
 from datetime import datetime
 
-def report_list():
-    return module_available = ['attendance', 'user']
 
 def generate_attend_report(class_pk, attend_date):
     user_list = tuple(i.user for i in Class_code.objects.get(pk=class_pk).class_assignment_set.all())
