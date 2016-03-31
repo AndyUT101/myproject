8c429fa24f0428adecf287b79672cbbd47192976
worminfo <worminfo@gmail.com>
2016-04-01 04:56:38 +0800

    update report and attendance

---
+++ attendance/utils.py
@@ -0,0 +1,65 @@
+from .models import *
+from user.models import User, Class_assignment, Class_code, Role
+
+from datetime import date, time, timedelta
+
+def list_attendance(class_code, date_val):
+    att_list = []
+    current_rule = current_rule(class_code)
+    date_min = datetime.combine(date_val, time.min)
+    date_max = datetime.combine(date_val, time.max)
+
+    class_obj = Class_code.objects.get(class_name = class_code)
+    user_list = [i.user for  i in class_obj.class_assignment_set.all() if i.user.role == Role.objects.get(name='student')]
+
+    no_card_list = [i for i in user_list if not i.card_id == ""]
+    user_list = no_card_list.symmetric_difference(user_list)
+
+    abs_list = []
+    for u in user_list:
+        if Attandance.objects.filter(user= logged_datetime__range=(date_min, date_max)).count() == 0:
+            abs_list.append(u)
+    user_list = abs_list.symmetric_difference(user_list)
+
+    on_time_list = []
+
+    for u in user_list:
+        arrive_time = Attandance.objects.filter(user= u, logged_datetime__range=(date_min, date_max)).order_by('logged_datetime')[0].logged_datetime
+        if current_rule.start_time - arrive_time >= timedelta(0):
+            on_time_list.append(u)
+
+    user_list = on_time_list.symmetric_difference(user_list)
+    late_list = user_list
+
+    att_list = {}
+
+    for u in no_card_list:
+        class_number = Class_assignment.objects.get(user=u).class_number
+        att_list[class_number] = (class_number, u, 'No card')
+
+    for u in abs_list:
+        class_number = Class_assignment.objects.get(user=u).class_number
+        att_list[class_number] = (class_number, u, 'Absent')
+
+    for u in on_time_list:
+        class_number = Class_assignment.objects.get(user=u).class_number
+        att_list[class_number] = (class_number, u, 'On Time')
+
+    for u in late_list:
+        class_number = Class_assignment.objects.get(user=u).class_number
+        att_list[class_number] = (class_number, u, 'On Time')
+
+        
+    return att_list
+
+def current_rule(class_code):
+	# return Rule object
+    class_obj = Class_code.objects.get(class_name = class_code)
+    current_applied_rule = Applied_rule.objects.filter(class_code = class_obj, start_date__gte = date.today(), end_date__lte = date.today())
+    rule_index = {}
+    for i in current_applied_rule:
+        rule_index[i.rule.priority] = i
+
+    applied_rule = rule_index(sorted(rule_index)[-1])
+
+    return applied_rule.rule
--- report/views.py
+++ report/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponseBadRequest
 from django import forms
 from django.template import RequestContext
-from user.models import User
+from user.models import User, Class_assignment, Class_code, Role
 
 import pyexcel.ext.xlsx
 import django_excel as excel
@@ -26,6 +26,12 @@ def generate_report(request):
     column_name = ['pk', 'username', 'firstname', 'lastname', 'role.name']
     return excel.make_response_from_query_sets(user_dataset, column_name, 'xlsx', file_name="custom")
 
+def generate_class_report(request, class_name):
+    user_class_dataset = Class_code.objects.get(class_name = class_name)
+    user_list = [i.user for  i in user_class_dataset.class_assignment_set.all() if i.user.role == Role.objects.get(name='student')]
+    column_name = ['pk', 'username', 'firstname', 'lastname', 'role.name']
+    return excel.make_response_from_query_sets(user_dataset, column_name, 'xlsx', file_name=class_name.'_stu')
+
 def attend_form(request):
     form_obj = Report_attend()
 
@@ -34,6 +40,8 @@ def attend_form(request):
         if form_obj.is_valid():
             class_pk = form_obj['class_pk']
             attend_date = form_obj['attend_date']
+            user_dataset = User.objects.all()
+
 
 
     return render(request, 'home.html', {
