83f1cb3c8b97449d16c652526e5ab276d16fabe2
worminfo <worminfo@gmail.com>
2016-04-01 04:58:26 +0800

    production enable

--- schoolpro/settings.py
+++ schoolpro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@!kim3y01!zio(@&@5#&nzox=*+&ns+m-5z-g@6_48iqix44k6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
