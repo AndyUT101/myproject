1027ccc0d13109b3d1d2573eaa1a876d6bdb5f4d
worminfo <worminfo@gmail.com>
2016-04-16 22:24:57 +0800

    s

--- report/views.py
+++ report/views.py
@@ -69,7 +69,7 @@ def attend_form(request):
     if request.method == 'POST':
         form_obj = Report_attend(request.POST)
         if form_obj.is_valid():
-            class_pk = form_obj['class_pk']
+            class_pk = form_obj['class_code']
             attend_date = form_obj['attend_date']
             return HttpResponseRedirect(reverse('attend_export'))
 
@@ -86,6 +86,6 @@ def attend_form(request):
         },
     })
 
-def attend_export(request, attend_date, class_pk):
+def attend_export(request, attend_date, class_code):
     pass
     
