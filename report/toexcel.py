f452c316e4ddcfaf870db2360838fe5ab8f13571
worminfo <worminfo@gmail.com>
2016-04-17 13:06:46 +0800

    x7

--- report/toexcel.py
+++ report/toexcel.py
@@ -136,7 +136,7 @@ def all_student(class_contents):
     for index, key in enumerate(class_contents):
         row = 5 + index
         output_data = class_contents
-        worksheet_s.write(row, 1, class_code, header)
+        worksheet_s.write(row, 1, output_data[0], header)
         worksheet_s.write(row, 2, output_data[0], header)
         worksheet_s.write(row, 3, output_data[1].firstname + " " + output_data[1].lastname, header)
         worksheet_s.write(row, 4, output_data[2], header)
