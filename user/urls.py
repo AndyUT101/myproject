81848a50bd43734cce6ec2a469e2bc6215158382
worminfo <worminfo@gmail.com>
2016-04-19 17:59:04 +0800

    zzz

--- user/urls.py
+++ user/urls.py
@@ -15,13 +15,12 @@ urlpatterns = [
     url(r'^modify/(?P<username>.+)/$', views.modify_user, name='modify_user'),
     url(r'^$', views.list_user, name='list_user'),
 
-    url(r'^class/delete/(?P<class_code>.+)/(?P<user_id>[0-9]+)/$', views.remove_classmember, name='remove_classmember'),
-
     url(r'^class/$', views.list_class, name='list_class'),
     url(r'^class/add/$', views.create_class, name='create_class'),
     url(r'^class/modify/(?P<class_code>.+)/$', views.modify_class, name='modify_class'),
     url(r'^class/delete/(?P<class_code>.+)/$', views.delete_class, name='delete_class'),
 
+    url(r'^class/delete/(?P<class_code>.+)/(?P<user_id>[0-9]+)/$', views.remove_classmember, name='remove_classmember'),
 
     url(r'^(?P<username>.+)/$', views.view_user, name='view_user'),
 ]
